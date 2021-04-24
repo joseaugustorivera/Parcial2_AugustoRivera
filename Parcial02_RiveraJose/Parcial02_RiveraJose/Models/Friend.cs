9b7bd845d7b078c3a8bcb345b08c8180d653db43
Jose Augusto Rivera <a2017115231@estudiantes.upsa.edu.bo>
2021-04-24 09:59:11 -0400

    He creado el modelo Friend

---
+++ Parcial02_RiveraJose/Parcial02_RiveraJose/Models/Friend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial02_RiveraJose.Models
+{
+    public class Friend
+    {
+        [Key]
+        public int FriendID { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BirthDay { get; set; }
+    }
+}
