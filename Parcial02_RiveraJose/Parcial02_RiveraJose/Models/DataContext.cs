e7abc183b57f84a54a8d64622f20fbf447098ea8
Jose Augusto Rivera <a2017115231@estudiantes.upsa.edu.bo>
2021-04-24 10:15:10 -0400

    Se ha configurado el WebConfig y el DataContext de nuestro modelo.

---
+++ Parcial02_RiveraJose/Parcial02_RiveraJose/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Parcial02_RiveraJose.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
